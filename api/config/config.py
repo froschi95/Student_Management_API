67867344959323357e89093122ef1fc77347c97b
froschi95 <franksuccess95@gmail.com>
2023-03-25 23:52:49 +0100

    Update config

--- api/config/config.py
+++ api/config/config.py
@@ -1,10 +1,14 @@
 import os
+import re
 from decouple import config
 from datetime import timedelta
 
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+uri = config('DATABASE_URL')  # or other relevant config var
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
 
 class Config:
     SECRET_KEY = config('SECRET_KEY', 'secret')
@@ -28,7 +32,9 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = config('DEBUG', False, cast=bool)
 
 
 config_dict = {
